dc18c7a671c6ecc45f063d4e91e09a27a2c23679
FRJ74 <frankr74@gmail.com>
2024-06-13 15:52:15 -0600

    Get the results of the round by building out the getRoundResults function.

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ function getRandomComputerResult() {
 }
   
 function hasPlayerWonTheRound(player, computer) {
-  return (player === "Rock" && computer === "Scissors" ||
-          player === "Scissors" && computer === "Paper" ||
-          player === "Paper" && computer ===  "Rock" ) 
+  return ( 
+    (player === "Rock" && computer === "Scissors")  ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer ===  "Rock" ) 
+  );
+  }
+
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+    if (hasPlayerWonTheRound === true) {
+      playerScore ++;
+      return `"Player wins!" ${userOption} " beats " ${computerResult} " !"`
+    } else if (hasPlayerWonTheRound === !true || !false) {
+      return `It's a tie! Both chose  ${userOption} !`
+    } else {
+      return `"Computer wins! " ${computerResult} " beats " ${userOption} " !"`
+      computerScore ++;
+    }
+   
   }
